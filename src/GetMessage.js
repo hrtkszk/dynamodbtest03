d4653fbf1502ae13b4ecf76e41859ecb51cc4d3f
haru <hrtkszk@gmail.com>
2021-11-21 07:54:06 +0900

    Add getmessage

--- src/GetMessage.js
+++ src/GetMessage.js
@@ -1,31 +1,36 @@
 import React from "react";
-import Button from './Button';
 
 class GetMessage extends React.Component {
   state = {
-    receivedMessages: ''
+      receivedMessages: '',
+      Messages: [
+        {sort_key: 9, secondary_local01: "2021-11-14 15:04:45.971405+09:00", partition_key: 2},
+        {sort_key: 10, partition_key: 2}
+      ]
   }
 
-  seeMsg = () => {
+  componentDidMount() {
     const requestOptions ={
-      method: 'GET',
-      headers:{'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}
+      method: 'POST',
+      headers:{'Content-Type': 'application/json'},
+      body: JSON.stringify({"partition_key1":2})
     };
-      fetch('https://769qg6p65h.execute-api.ap-northeast-1.amazonaws.com/dbread?partition_key1=1', requestOptions)
-      .then((response)=> response.json())
-      .then((responseJson) =>{
-        console.log(responseJson)
-      })
+    fetch("https://769qg6p65h.execute-api.ap-northeast-1.amazonaws.com/dbread",requestOptions)
+    .then(response=> response.json())
+    .then(result =>{
+      console.log(result)
+      this.setState({receivedMessages:result.body})
+      console.log(this.state.receivedMessages)
+      console.log(this.state.Messages)
+    })
+
   }
 
   render() {
+    
     return (
       <div>
-        <Button
-          title="SeeMessage"
-          onClick={this.seeMsg}
-        />
-        {this.state.receivedMessages}
+
       </div>
     )
   }
