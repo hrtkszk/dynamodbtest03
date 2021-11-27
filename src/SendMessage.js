007678daeba39e52c1a43eabd0ec9df93ef30b99
haru <hrtkszk@gmail.com>
2021-11-27 15:47:47 +0900

    Change sendmessage id and sendto id

--- src/SendMessage.js
+++ src/SendMessage.js
@@ -4,7 +4,8 @@ import Button from './Button';
 
 class SendMessage extends React.Component {
     state = {
-      message: ''
+      message: '',
+      image:''
     }
     onChange = (key, value) => {
       //this.props.updateErrorMessage(null)
@@ -16,7 +17,7 @@ class SendMessage extends React.Component {
       const requestOptions ={
         method: 'POST',
         headers:{'Content-Type': 'application/json'},
-        body: JSON.stringify({"partition_key1":this.state.message,"partition_key2":"2"})
+        body: JSON.stringify({"id":"aaaa","sendTo":"bbbb","message":this.state.message,"image":this.state.image})
       };
       fetch("https://skbwb0u423.execute-api.ap-northeast-1.amazonaws.com/dev_amp_db",requestOptions)
       .then((response)=> response.json())
